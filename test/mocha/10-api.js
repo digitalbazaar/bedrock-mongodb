ff8358be39ab6d423b02c95d797c8dd2f1ad06bd
andrewjones <ajones@digitalbazaar.com>
2020-05-18 15:26:35 -0400

    Add basic tests.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -1,3 +1,32 @@
-describe('api', () => {
-  it('should work');
+const {promisify} = require('util');
+const database = require('bedrock-mongodb');
+
+describe('api', function() {
+  describe('openCollection', function() {
+    it('should open a collection', async function() {
+      let error = null;
+      try {
+        await promisify(database.openCollections)(['test']);
+      } catch(e) {
+        error = e;
+      }
+      should.not.exist(error);
+    });
+  });
+  describe('createIndex', function() {
+    it('should create an index', async function() {
+      let error = null;
+      try {
+        await promisify(database.openCollections)(['test']);
+        await promisify(database.createIndexes)([{
+          collection: 'test',
+          fields: {id: 1},
+          options: {unique: true, background: false}
+        }]);
+      } catch(e) {
+        error = e;
+      }
+      should.not.exist(error);
+    });
+  });
 });
