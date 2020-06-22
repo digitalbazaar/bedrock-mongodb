3ef62964e33e0b405db522a6d19925c0faf96ab1
Andrew Jones <ajones@digitalbazaar.com>
2020-06-22 13:30:35 -0400

    Reuse config.username and config.password.

--- test/test.config.js
+++ test/test.config.js
@@ -12,22 +12,15 @@ config.mongodb.host = process.env.MONGODB_HOST || 'localhost';
 config.mongodb.port = process.env.MONGODB_PORT || 27017;
 config.mongodb.connectOptions.replicaSet = process.env.MONGODB_REPLICASET;
 if(process.env.MONGODB_USERNAME && process.env.MONGODB_PASSWORD) {
+  config.mongodb.username = process.env.MONGODB_USERNAME;
+  config.mongodb.password = process.env.MONGODB_PASSWORD;
   const {connectOptions} = config.mongodb;
   connectOptions.ssl = true;
-  connectOptions.auth = {
-    user: process.env.MONGODB_USERNAME || 'admin',
-    password: process.env.MONGODB_PASSWORD || 'admin'
-  };
   connectOptions.authSource = process.env.MONGODB_AUTHSOURCE || 'admin';
 }
 //config.mongodb.connectOptions.loggerLevel = 'debug';
 config.mongodb.dropCollections.onInit = true;
 config.mongodb.dropCollections.collections = [];
-/**
-config.mongodb.authentication = {
-  authMechanism: 'SCRAM-SHA-1'
-};
-*/
 config.mongodb.forceAuthentication = true;
 
 config.mocha.tests.push(path.join(__dirname, 'mocha'));
