a2d94faac144e40a6958f3d88c34a10c51b40695
Andrew Jones <ajones@digitalbazaar.com>
2020-06-23 09:33:18 -0400

    Restore test.config.js settings.

--- test/test.config.js
+++ test/test.config.js
@@ -7,16 +7,18 @@ const {config} = require('bedrock');
 const path = require('path');
 
 // MongoDB
-config.mongodb.name = 'test-connectOptions';
+config.mongodb.name = 'bedrock_mongodb_test';
 config.mongodb.host = process.env.MONGODB_HOST || 'localhost';
 config.mongodb.port = process.env.MONGODB_PORT || 27017;
-config.mongodb.connectOptions.replicaSet = process.env.MONGODB_REPLICASET;
 if(process.env.MONGODB_USERNAME && process.env.MONGODB_PASSWORD) {
   config.mongodb.username = process.env.MONGODB_USERNAME;
   config.mongodb.password = process.env.MONGODB_PASSWORD;
   const {connectOptions} = config.mongodb;
+  // this can be false
   connectOptions.ssl = true;
   connectOptions.authSource = process.env.MONGODB_AUTHSOURCE || 'admin';
+  // this can safely be undefined
+  config.mongodb.connectOptions.replicaSet = process.env.MONGODB_REPLICASET;
 }
 //config.mongodb.connectOptions.loggerLevel = 'debug';
 config.mongodb.dropCollections.onInit = true;
