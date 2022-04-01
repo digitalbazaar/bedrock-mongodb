2cf004b97440bea50378709b40e12e6f348867b3
Dave Longley <dlongley@digitalbazaar.com>
2022-04-01 00:57:44 -0400

    Fix linting error.

--- test/test.js
+++ test/test.js
@@ -5,16 +5,4 @@ import * as bedrock from '@bedrock/core';
 import '@bedrock/mongodb';
 import '@bedrock/test';
 
-/*try {
-  await import('/work/src/bedrock-dev/bedrock-mongodb/test/mocha/10-api.js');
-} catch(e) {
-  console.log('e', e);
-}
-
-try {
-  await import('file:///work/src/bedrock-dev/bedrock-mongodb/test/mocha/10-api.js');
-} catch(e) {
-  console.log('e', e);
-}*/
-
 bedrock.start().catch(err => console.error(err));
