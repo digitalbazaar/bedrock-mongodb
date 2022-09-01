6c0f72b33e795e731946545c2db7c43157bc5fc7
Dave Longley <dlongley@digitalbazaar.com>
2022-09-01 17:06:18 -0400

    Update copyright.

--- test/test.config.js
+++ test/test.config.js
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
+/*!
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
 import {fileURLToPath} from 'node:url';
